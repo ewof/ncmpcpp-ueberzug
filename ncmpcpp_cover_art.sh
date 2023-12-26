f203badff9149d6547b304d76b24fb2896f528ba
ewof <marroq@marroq.xyz>
2023-12-26 01:07:54 -0500

    Update ncmpcpp_cover_art.sh

--- ncmpcpp_cover_art.sh
+++ ncmpcpp_cover_art.sh
@@ -2,7 +2,7 @@
 # Cover art script for ncmpcpp-ueberzug
 
 # SETTINGS
-music_library="$HOME/Music"
+music_library="$HOME/media/Music"
 fallback_image="$HOME/.config/ncmpcpp/ncmpcpp-ueberzug/img/fallback.png"
 padding_top=24
 padding_bottom=2
